da164b34462ed93b144b644e652ecae4d41fa7bb
gibert <gibert@abes.fr>
2024-01-16 17:55:14 +0100

    ajout doc openApi + suppression création image ccsd

--- src/main/java/fr/abes/theses/diffusion/controller/DiffusionController.java
+++ src/main/java/fr/abes/theses/diffusion/controller/DiffusionController.java
@@ -6,7 +6,9 @@ import fr.abes.theses.diffusion.database.These;
 import fr.abes.theses.diffusion.service.Diffusion;
 import fr.abes.theses.diffusion.service.VerificationDroits;
 import fr.abes.theses.diffusion.utils.TypeRestriction;
-import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -37,13 +39,20 @@ public class DiffusionController {
      * @return
      * @throws Exception
      */
+
     /*
+    @Operation(
+            summary = "Renvoie un fichier de thèse (ou une liste HTML de liens si la thèse est constituée de plusieurs fichiers) en accès restreint",
+            description = "Renvoie les thèses disponibles pour les membres de l'Enseignement Supérieur et de la Recherche après authentification via la fédération d'identité Renater")
+    @ApiResponse(responseCode = "200", description = "Opération terminée avec succès, le fichier de thèse est renvoyé")
+    @ApiResponse(responseCode = "400", description = "Le format du numéro national de thèse fourni est incorrect")
+    @ApiResponse(responseCode = "403", description = "Accès refusé")
     @GetMapping(value = "document/protected/{nnt}")
     public ResponseEntity<byte[]> documentProtected(
             @PathVariable
-            @ApiParam(name = "nnt", value = "nnt de la thèse", example = "2023MON12345") String nnt, HttpServletResponse response) throws Exception {
+            @Parameter(name = "nnt", description = "Numéro National de Thèse", example = "2013MON30092") String nnt, HttpServletResponse response) throws Exception {
 
-        log.info("protection passée pour ".concat(nnt));
+        log.debug("protection passée pour ".concat(nnt));
         if (!service.verifieNnt(nnt)) {
             log.error("nnt incorrect");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -59,18 +68,23 @@ public class DiffusionController {
         }
         return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
-    */
-
+*/
     /**
      * Renvoie les thèses disponibles en accès libre
      * @param nnt
      * @return
      * @throws Exception
      */
+    @Operation(
+            summary = "Renvoie un fichier de thèse en accès libre",
+            description = "Renvoie les thèses en accès libre")
+    @ApiResponse(responseCode = "200", description = "Opération terminée avec succès, le fichier de thèse est renvoyé")
+    @ApiResponse(responseCode = "400", description = "Le format du numéro national de thèse fourni est incorrect")
+    @ApiResponse(responseCode = "403", description = "Accès refusé")
     @GetMapping(value = "document/{nnt}")
     public ResponseEntity<byte[]> document(
             @PathVariable
-            @ApiParam(name = "nnt", value = "nnt de la thèse", example = "2023MON12345") String nnt, HttpServletResponse response) throws Exception {
+            @Parameter(name = "nnt", description = "Numéro National de Thèse", example = "2013MON30092") String nnt, HttpServletResponse response) throws Exception {
 
         if (!service.verifieNnt(nnt)) {
             log.error("nnt incorrect");
@@ -112,10 +126,17 @@ public class DiffusionController {
      * @return
      * @throws Exception
      */
+    @Operation(
+            summary = "Redirige sur l'intranet de l'établissement où est disponible le fichier de thèse",
+            description = "il faut disposer d'un compte local à l'établissement pour accéder au fichier de la thèse")
+    @ApiResponse(responseCode = "200", description = "Opération terminée avec succès, redirection sur l'intranet de l'établissement")
+    @ApiResponse(responseCode = "400", description = "Le format du numéro national de thèse fourni est incorrect")
+    @ApiResponse(responseCode = "403", description = "Accès refusé")
+    @ApiResponse(responseCode = "404", description = "L'url de la thèse n'est pas trouvée dans les métadonnées de description de la thèse (TEF) ou l'url ne répond pas")
     @GetMapping(value = "document/intranetEtab/{nnt}")
     public ResponseEntity<byte[]> documentIntranetEtab(
             @PathVariable
-            @ApiParam(name = "nnt", value = "nnt de la thèse", example = "2023MON12345") String nnt, HttpServletResponse response) throws Exception {
+            @Parameter(name = "nnt", description = "Numéro National de Thèse", example = "2013MON30092") String nnt, HttpServletResponse response) throws Exception {
 
         if (!service.verifieNnt(nnt)) {
             log.error("nnt incorrect");
@@ -127,16 +148,20 @@ public class DiffusionController {
 
         // cas 4 intranet, renvoie sur l'intranet de l'établissement si l'url est renseignée et répond (url dans les identifier)
         if (verificationDroits.getScenario(these.getTef(), nnt).equals("cas4")) {
-            if (diffusion.diffusionEtablissementIntranet(these.getTef()))
+            if (diffusion.diffusionEtablissementIntranet(these.getTef())) {
                 diffusion.redirectionEtablissementIntranet(these.getTef(), response);
+                return ResponseEntity.status(HttpStatus.OK).build();
+            }
             else
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
 
         // cas 6 intranet, renvoie sur l'intranet de l'établissement si l'url est renseignée et répond (url dans le bloc de gestion du tef)
         if (verificationDroits.getScenario(these.getTef(), nnt).equals("cas6")) {
-            if (diffusion.diffusionEtablissementAvecUneSeuleUrl(these.getTef(), nnt))
+            if (diffusion.diffusionEtablissementAvecUneSeuleUrl(these.getTef(), nnt)) {
                 diffusion.redirectionEtabAvecUneSeuleUrl(these.getTef(), response, false);
+                return ResponseEntity.status(HttpStatus.OK).build();
+            }
             else
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
@@ -150,12 +175,19 @@ public class DiffusionController {
      * @return
      * @throws Exception
      */
+    @Operation(
+            summary = "Fournit un lien d'accès direct au fichier de thèse (ou de l'une de ses annexes) si la thèse est disponible en accès libre",
+            description = "permet de télécharger le ou les fichiers de la thèse depuis la plateforme Abes")
+    @ApiResponse(responseCode = "200", description = "Opération terminée avec succès, redirection sur l'intranet de l'établissement")
+    @ApiResponse(responseCode = "400", description = "Le format du numéro national de thèse fourni est incorrect")
+    @ApiResponse(responseCode = "403", description = "Accès refusé")
     @GetMapping(value = "document/{nnt}/{nomFichierAvecCheminLocal}")
     public ResponseEntity<byte[]> accesDirectAuFichier(
             @PathVariable
-            @ApiParam(name = "nnt", value = "nnt de la thèse", example = "2023MON12345") String nnt,
+            @Parameter(name = "nnt", description = "Numéro National de Thèse", example = "2013MON30092") String nnt,
             @PathVariable
-            @ApiParam(name = "nomFichierAvecCheminLocal", value = "chemin local vers le fichier de thèse ou de l'une de ses annexes", example = "/0/0/these.pdf") String nomFichierAvecCheminLocal,
+            @Parameter(name = "nomFichierAvecCheminLocal", description = "chemin local vers le fichier de thèse ou de l'une de ses annexes", example = "/0/0/these.pdf")
+            String nomFichierAvecCheminLocal,
             HttpServletResponse response) throws Exception {
 
         if (!service.verifieNnt(nnt)) {
